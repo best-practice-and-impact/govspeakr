e7cec81f8dff36a7c5b0579b71c2f329040c6ecb
David Foster <31405412+foster999@users.noreply.github.com>
2019-11-01 11:11:24 +0000

    Refactored and added functions to utils.R
    Also regenerated docs

--- R/utils.R
+++ R/utils.R
@@ -1,55 +1,112 @@
 #' Converts image file names to a dataframe, with a field containing the
-#' original image name and one for the Whitehall publishing software
-#' @param imgs character vector of png files to be converted to Whitehall format
+#' original image name and corresponding govdown reference
+#' @param imgs character vector of png files to be referenced in govdown format
 #'   (!!n). The filename must start and end with a number and have text in
 #'   between (eg, "1 abcd 1.png")
-#' @title Generate image numbers
-generate_image_numbers <- function(imgs) {
-  generate_image_numbers <- data.frame(image = imgs)
-  generate_image_numbers$img_number <- gsub("\\.png$", "", generate_image_numbers$image)
-  generate_image_numbers$pre_dec <- gsub("([0-9]+).*$", "\\1", generate_image_numbers$img_number)
-  generate_image_numbers$post_dec <- gsub(".*([0-9])$", "\\1", generate_image_numbers$img_number)
-  generate_image_numbers$combined <- as.numeric(paste0(generate_image_numbers$pre_dec,
-                                                       ".",
-                                                       generate_image_numbers$post_dec))
-  generate_image_numbers$img_number <- paste0("!!", rank(generate_image_numbers$combined))
+#' @name generate_image_references
+#' @title Generate govdown image references
+generate_image_references <- function(img_files) {
+  image_references <- data.frame(image_file = img_files)
+  image_references$image_reference <- gsub("\\.png$", "", image_references$image_file)
+  image_references$pre_dec <- gsub("([0-9]+).*$", "\\1", image_references$image_reference)
+  image_references$post_dec <- gsub(".*([0-9])$", "\\1", image_references$image_reference)
+  image_references$combined <- as.numeric(paste0(image_references$pre_dec,
+                                                 ".",
+                                                 image_references$post_dec))
+  image_references$image_reference <- paste0("!!", rank(image_references$combined))
 
-
-  generate_image_numbers <- generate_image_numbers[, c("image", "img_number")]
-  return(generate_image_numbers)
-
+  image_references <- image_references[, c("image_file", "image_reference")]
+  return(image_references)
 }
 
-#' Convert markdown file to Whitehall (gov.uk) govspeak markdown publishing format
-#' @param path string; filename (including path) to *.md file for conversion
-#' @param images_folder string; folder containing images for *.md file. Defaults
-#'   to "images"
-#' @export
-#' @name convert_md
-#' @title Convert markdown file to Whitehall publishing format
-convert_md <- function(path, images_folder = "images") {
-  md_file <- readLines(path)
 
-  imgs <- list.files(paste0(dirname(path),
-                            "/",
-                            images_folder))
-  imgs <- generate_image_numbers(imgs)
-
-  for (i in seq_along(imgs$image)) {
-    orig <- imgs$image[i]
+#' Convert markdown image references to govspeak format (!!n)
+#' @param image_references dataframe of image file names and associated govdown
+#'   reference.
+#' @param md_file string with markdown file text
+#' @param images_folder string; folder containing images for *.md file
+#' @name convert_image_references
+#' @title Convert markdown image references to govdown
+convert_image_references <- function(image_references, md_file, images_folder) {
+  govspeak_image_reference_file <- as.character(md_file)
+  for (i in seq_along(image_references$image_file)) {
+    file_name <- image_references$image_file[i]
     md_image_format <- paste0("!\\[\\]\\(",
                               images_folder,
                               "/",
-                              orig,
+                              file_name,
                               "\\)<!-- -->")
-    targ <- paste0(as.character(imgs[i, 2]),
-                   "\n")
+    
+    govspeak_reference <- paste0(as.character(image_references$image_reference[i]),
+                                 "\n")
 
-    md_file <- gsub(md_image_format,
-                    targ,
-                    md_file)
+    govspeak_image_reference_file <- gsub(md_image_format,
+                                          govspeak_reference,
+                                          govspeak_image_reference_file)
   }
+  return(govspeak_image_reference_file)
+}
 
-  write(md_file, gsub("\\.md", "_converted\\.md", path))
 
+#' Convert markdown callout sytax to govspeak
+#' @param md_file string; markdown file text
+#' @name convert_callouts
+#' @title Convert markdown callout syntax to govspeak
+convert_callouts <- function(md_file) {
+  # Lazy match on lines starting with ">", which are then flanked with "^"
+  # Currently only catches single line callouts
+  converted_md_file <- gsub("(\\n)>[ ]*(.*?\\n)", "\\1^\\2", md_file)
+}
+
+
+#' Replace R markdown header with title only
+#' @param md_file string; markdown file text
+#' @name convert_title
+#' @title Replace R markdown header with ## title
+convert_title <- function(md_file) {
+  # Lazy match on header to extract title
+  # Remove substitution if titles must be entered manually
+  cleaned_md_file <- gsub("---\\n.*?title:.*\"(.*)\".*?---\\n", "# \\1", md_file)
+}
+
+
+#' Remove R markdown multiline block elements (package warnings, but also multiline code blocks)
+#' @param md_file string; markdown file text
+#' @name remove_rmd_blocks
+#' @title Remove R markdown multiline block elements (package warning and code block)
+remove_rmd_blocks <- function(md_file) {
+  # Lazy match on warnings and code blocks
+  cleaned_md_file <- gsub("```\\n.*?```\\n\\n", "", md_file)
+}
+
+
+#' Convert markdown file to Whitehall publisher (gov.uk) govspeak markdown format
+#' @param path string; filename (including path) to *.md file for conversion
+#' @param images_folder string; folder containing images for *.md file. Defaults
+#'   to "images"
+#' @param remove_blocks bool; decision to remove block elements from *.md file
+#' @export
+#' @name convert_md
+#' @title Convert standard markdown file to govspeak
+convert_md <- function(path, images_folder = "images", remove_blocks=FALSE) {
+  md_file <- paste(readLines(path), collapse = "\n")
+
+  img_files <- list.files(paste0(dirname(path),
+                                 "/",
+                                 images_folder))
+
+  image_references <- generate_image_references(img_files)
+  govspeak_file <- convert_image_references(image_references,
+                                            md_file,
+                                            images_folder)
+  
+  govspeak_file <- convert_title(govspeak_file)
+  
+  govspeak_file <- convert_callouts(govspeak_file)
+  
+  if (remove_blocks) {
+    govspeak_file <- remove_rmd_blocks(govspeak_file)
+  }
+  
+  write(govspeak_file, gsub("\\.md", "_converted\\.md", path))
 }
--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # govspeakr
-> **WARNING**: Please note that this package is currently under development. To be updated when this package is ready for use,
-or to contribute, please get in touch.
+**WARNING**: Please note that this package is currently under development. To be updated when this package is ready for use, or to contribute, please get in touch.
 
 ## What is govspeakr?
 govspeakr is an R package that is designed to help users to develop
@@ -8,7 +7,7 @@ govspeakr is an R package that is designed to help users to develop
 Specifically, it enables the user to convert markdown files to [govspeak markdown](http://govspeak-preview.herokuapp.com/guide),
 which can be directly uploaded to the Whitehall publisher ([gov.uk](https://www.gov.uk)).
 
-Govspeak markup uses a different notation for image location within the text, where images are numbered in order of appearance:
+Govspeak markup uses a different notation for image location within the text:
 ```
 Figure 1
 !!1
@@ -31,8 +30,8 @@ Figure 2
 ## Usage
 
 The conversion acts on a markdown (*.md) file only, so Rmarkdown (*.Rmd) should first be converted to *.md.
-This can be achieved using the YAML at the top of a *.Rmd file, either outputing `md_document` or keeping the *.md produced
-when outputting a htlm or pdf document (`keep_md: true`):
+This can be achieved using the YAML at the top of a *.Rmd file, either outputing md_document or keeping the *.md produced
+when outputting a htlm or pdf document:
 
 
 ```
@@ -42,11 +41,14 @@ output:
   html_document:
     keep_md: true
 ---
+
+
 ```
 
-The `govspeakr::convert_md()` function accepts a path to a *.md file, along with the name of the subdirectory that contains the
-images required to produce the document.The images_folder parameter defaults to "images/", so other directory names must be specified
-as below:
+The `govspeakr::convert_md()` function accepts a path to a *.md file, along with the name of the subdirectory that contains any required images.
+This subdirectory should only include the images required for the document. The images_folder parameter defaults to "images/",
+so other names must be specified as below:
+
 
 ```
 convert_md("C:Users/me/publications/statistical_publication.md", images_folder="figures")
