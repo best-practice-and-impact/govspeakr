131462f9019db614827c08308afae4c7ae7fb628
David Foster <foster.dev999@gmail.com>
2020-04-30 15:36:34 +0100

    Added logo and updated docs

--- R/api_functions.R
+++ R/api_functions.R
@@ -1,4 +1,5 @@
-#' Convert markdown file to Whitehall Publisher (GOV.UK) govspeak markdown format
+#' Convert a Markdown file to the Whitehall Publisher (GOV.UK) govspeak
+#'   markup format
 #' @param path string; filename (including path) to *.md file for conversion
 #' @param images_folder string; folder containing images for *.md file. Defaults
 #'   to "images"
@@ -12,23 +13,23 @@ convert_md <- function(path,
                        remove_blocks=FALSE
                        ) {
   md_file <- paste(readLines(path), collapse = "\n")
-  
+
   img_files <- list.files(paste0(dirname(path),
                                  "/",
                                  images_folder))
-  
+
   image_references <- generate_image_references(img_files)
   govspeak_file <- convert_image_references(image_references,
                                             md_file,
                                             images_folder)
-  
+
   govspeak_file <- remove_header(govspeak_file)
-  
+
   govspeak_file <- convert_callouts(govspeak_file)
-  
+
   if (remove_blocks) {
     govspeak_file <- remove_rmd_blocks(govspeak_file)
   }
-  
+
   write(govspeak_file, gsub("\\.md", "_converted\\.md", path))
 }
--- R/utility_functions.R
+++ R/utility_functions.R
@@ -1,28 +1,28 @@
 #' Converts image file names to a dataframe, with a field containing the
-#' original image name and corresponding govdown reference
-#' @param img_filenames character vector of files to be referenced in govdown format
-#'   (!!n). The filename must start and end with a number and have text in
-#'   between (eg, "1-abcd-1.png")
+#'   original image name and corresponding govspeak reference
+#' @param img_filenames character vector of files to be referenced in
+#'   govspeak format (e.g. !!n). The filename must start and end with
+#'   a number and have text in between (eg, "1-abcd-1.png")
 #' @name generate_image_references
-#' @title Generate govdown image references
+#' @title Generate govspeak image references
 generate_image_references <- function(img_filenames) {
   image_references <- data.frame(image_file = img_filenames)
   # Strip ext - not image file specific
   image_references$image_reference <- tools::file_path_sans_ext(image_references$image_file)
-  
+
   # Capture chunk number and image position within chunk
   if (!all(as.logical(lapply(image_references$image_file, stringr::str_detect, "^[0-9]")))) {
     stop("image chunk names must start with a number, which should correspond to their order in the .Rmd file")
   }
   image_references$pre_dec <- gsub("([0-9]+).*$", "\\1", image_references$image_reference)
   image_references$post_dec <- gsub(".*([0-9]+)$", "\\1", image_references$image_reference)
-  
+
   # Convert to decimal for ranking
   image_references$combined <- as.numeric(paste0(image_references$pre_dec,
                                                  ".",
                                                  image_references$post_dec))
   image_references$image_reference <- paste0("!!", rank(image_references$combined))
-  
+
   # Keep mapping of image files to govspeak references
   image_references <- image_references[, c("image_file", "image_reference")]
   return(image_references)
@@ -30,27 +30,27 @@ generate_image_references <- function(img_filenames) {
 
 
 #' Convert markdown image references to govspeak format (!!n)
-#' @param image_references dataframe of image file names and associated govdown
-#'   reference.
+#' @param image_references dataframe of image file names and
+#'   associated govspeak reference.
 #' @param md_file string with markdown file text
 #' @param images_folder string; folder containing images for *.md file
 #' @name convert_image_references
-#' @title Convert markdown image references to govdown
+#' @title Convert markdown image references to govspeak
 convert_image_references <- function(image_references, md_file, images_folder) {
   govspeak_image_reference_file <- as.character(md_file)
   for (i in seq_along(image_references$image_file)) {
     file_name <- image_references$image_file[i]
-    
+
     # Construct markdown reference to image file
     md_image_format <- paste0("!\\[\\]\\(",
                               images_folder,
                               "/",
                               file_name,
                               "\\)<!-- -->")
-    
+
     govspeak_reference <- paste0(as.character(image_references$image_reference[i]),
                                  "\n")
-    
+
     # Replace markdown image reference with govspeak reference
     govspeak_image_reference_file <- gsub(md_image_format,
                                           govspeak_reference,
@@ -68,6 +68,7 @@ convert_callouts <- function(md_file) {
   # Lazy match on lines starting with ">", which are then flanked with "^"
   # Currently only catches single line callouts
   converted_md_file <- gsub("(\\n)>[ ]*(.*?\\n)", "\\1^\\2", md_file)
+  return(converted_md_file)
 }
 
 
@@ -83,10 +84,12 @@ remove_header <- function(md_file) {
 }
 
 
-#' Remove R markdown multiline block elements (package warnings, but also multiline code blocks)
+#' Remove R markdown multiline block elements (package warnings,
+#'   but also multiline code blocks)
 #' @param md_file string; markdown file text
 #' @name remove_rmd_blocks
-#' @title Remove R markdown multiline block elements (package warning and code block)
+#' @title Remove R markdown multiline block elements (package
+#'   warning and code block)
 remove_rmd_blocks <- function(md_file) {
   # Lazy match on warnings and code blocks
   cleaned_md_file <- gsub("```\\n.*?```\\n\\n", "", md_file)
